809e433680ae3f0e0b37defaad73af996fa30ac9
Mohd Hannan <mohdhannan7913@gmail.com>
2025-06-25 23:49:04 +0530

    add middleware (isAuthenticated)

---
+++ Backend/middleware/isAuthenticated.js
@@ -0,0 +1,27 @@
+
+import jwt from "jsonwebtoken"
+const isAuthenticated = async (req, res, next)=>{
+    try {
+        console.log("Enter in isAuthenticated")
+         const token = req.cookies.token;
+         if(!token){
+            res.status(400).json({message: "User is not Authenticated"})
+         }
+
+         const decode = await jwt.verify(token, process.env.JWT_SECRET)
+         console.log("decode = ", decode);
+
+         if(!decode){
+            res.status(400).json({message: "Invalid Token"})
+         }
+
+         req.id = decode.userId;
+         next();
+
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default isAuthenticated;
