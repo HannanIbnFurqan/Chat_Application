0942386023f80275764aab6d2f6d9063a0c0436a
Mohd Hannan <mohdhannan7913@gmail.com>
2025-07-01 00:25:29 +0530

    modified

--- Backend/index.js
+++ Backend/index.js
@@ -3,21 +3,25 @@ import dotenv from "dotenv";
 import userRoute from "./routes/userRouter.js"; // Correct filename
 import connectDB from "./config/database.js";
 import cookieParser from "cookie-parser";
+import aiRoute from "./routes/aiRoute.js";
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 6000;
 
-// ✅ Middleware to parse JSON
+//  Middleware to parse JSON
 app.use(express.json());
 app.use(cookieParser());
 
 
-// ✅ Connect DB before server starts
+// Connect DB before server starts
 connectDB();
 
-// ✅ Routes
+// Routes
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/ai", aiRoute); 
+
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT", PORT);
