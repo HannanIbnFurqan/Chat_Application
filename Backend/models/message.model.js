63a339b00b84342a2ad53d4e839e18ba060c974a
Mohd Hannan <mohdhannan7913@gmail.com>
2025-07-09 23:56:12 +0530

    complete message model

--- Backend/models/message.model.js
+++ Backend/models/message.model.js
@@ -1,23 +1,27 @@
 import mongoose from "mongoose";
 
-const messageModel = new mongoose.Schema({
-    senderId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    receiverId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    message:{
-        type: String,
-        required: true
-    }
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  messageType: {
+    type: String,
+    enum: ['text', 'image', 'file'],
+    default: 'text'
+  }
+}, { timestamps: true });
 
-})
+const Message = mongoose.model("Message", messageSchema);
 
-const Message = new mongoose.model("Message", messageModel)
-
-export default Message 
+export default Message;
